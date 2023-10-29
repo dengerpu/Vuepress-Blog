dc8c4f74200d127e3e738fc1676f02540f096177
dep <1649743146@qq.com>
2023-10-29 11:25:43 +0800

    feat: 新增百度定时链接推送

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=www.006969.xyz&token=O8KAN2eEK33k15Fe"
 
 rm -rf urls.txt # 删除文件

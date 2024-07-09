58a11ceda2058bccbaa898e7bff58b61d4db4c88
dep <1649743146@qq.com>
2024-07-09 23:12:16 +0800

    update: 更新文档

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ set -e
 
 
 push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:dengerpu/Vuepress-Blog.git
+
+
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
 push_branch=gh-pages # 推送的分支
